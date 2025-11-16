da87592e3c2a256124e16b97d368f9f91e8ef438
hoangduy0111 <lehoangduy344@gmail.com>
2025-11-16 23:13:31 +0700

    Tao trang cap nhat lien he

--- src/router/index.js
+++ src/router/index.js
@@ -2,35 +2,42 @@ import { createWebHistory, createRouter } from "vue-router";
 import ContactBook from "@/views/ContactBook.vue";
 
 const routes = [
-    // 1. Route cho trang chủ (Hiển thị danh sách liên hệ)
-    {
-        path: "/",
-        name: "contactbook",
-        component: ContactBook,
-    },
-    // 2. Route cho trang Thêm mới liên hệ
-    {
-        path: "/contacts/add",
-        name: "contact.add",
-        component: () => import("@/views/ContactAdd.vue"),
-    },
-    // 3. Route cho trang Không tìm thấy (Not Found)
-    {
-        path: "/:pathMatch(.*)*",
-        name: "notfound",
-        component: () => import("@/views/NotFound.vue"),
-    },
+  // 1. Route cho trang chủ (Hiển thị danh sách liên hệ)
+  {
+    path: "/",
+    name: "contactbook",
+    component: ContactBook,
+  },
+  // 2. Route cho trang Thêm mới liên hệ
+  {
+    path: "/contacts/add",
+    name: "contact.add",
+    component: () => import("@/views/ContactAdd.vue"),
+  },
+  // 3. Route cho trang Không tìm thấy (Not Found)
+  {
+    path: "/:pathMatch(.*)*",
+    name: "notfound",
+    component: () => import("@/views/NotFound.vue"),
+  },
 
-    {
-        path: "/:pathMatch(.*)*",
-        name: "notfound",
-        component: () => import("@/views/NotFound.vue"),
-    }
+  {
+    path: "/:pathMatch(.*)*",
+    name: "notfound",
+    component: () => import("@/views/NotFound.vue"),
+  },
+
+  {
+    path: "/contacts/:id",
+    name: "contact.edit",
+    component: () => import("@/views/ContactEdit.vue"),
+    props: true, // Truyền các biến trong $route.params vào làm props
+  },
 ];
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes,
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
 });
 
 export default router;
